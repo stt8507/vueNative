b67f199fe8a5af304e31ddcb0b2819c51b024365
samsonk88507 <stt8507@gmail.com>
2022-06-26 18:17:17 +0800

    init

---
+++ App.vue
@@ -0,0 +1,38 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">{{ message }}</text>
+    <button title="Press me!" @press="exclaim" />
+    <image
+          :style="{width: 50, height: 50}"
+          :source="{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}"
+        />
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World"
+    };
+  },
+  methods: {
+    exclaim() {
+      this.message += "!";
+    }
+  },
+};
+</script>
+
+<style>
+.container {
+  flex: 1;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+}
+.text-color-primary {
+  color: blue;
+  font-size: 30;
+}
+</style>
